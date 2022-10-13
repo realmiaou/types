9ed79543c05dec9a3b519a30fd576c868a0c3a7b
Thibaut Mottet <thibaut.mottet@gmail.com>
2022-10-13 17:11:51 +0000

    fix: import all types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+export * from './crypto'
+export * from './date'
+export * from './i18n'
+export * from './money'
+export * from './user'
+export * from './web'
+export * from './audit'
 export type Parameter<T extends (...args: any) => any> = Parameters<T>[0]
